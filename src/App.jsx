ef18cbe4c3943c22f11b2d02f697ed1d2430f791
Pavan Kumar <pavanmaddala16@gmail.com>
2023-10-09 14:36:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const inputRef = useRef(0);
+
+  const increment = () => {
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+  };
+
+  useEffect(() => {
+    console.log("current", inputRef.current);
+  }, []);
+
+  return (
+    <>
+    <input type="text" ref={inputRef} />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={increment}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
